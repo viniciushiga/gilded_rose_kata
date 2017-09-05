2394a462cdc5111f18a2d5dd9e6da3e277788eed
Vinicius Higa <viniciushiga@gmail.com>
2017-09-05 03:27:27 -0300

    Reduces the amount of created objects and uses more functions instead of void methods

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -6,107 +6,102 @@ end
 
 class ItemQualityUpdater
   def self.update_quality(item)
-    get_quality_updater_for(item).update_quality
+    calculator = get_quality_calculator_for(item)
+    item.sell_in = calculator.calculate_sell_in(item)
+    item.quality = calculator.calculate_quality(item)
   end
 
   private
 
-  def self.get_quality_updater_for(item)
-    case item.name
-    when /^Backstage/
-      BackstagePassQualityUpdater.new(item)
-    when /^Aged Brie/
-      AgedBrieQualityUpdater.new(item)
-    when /^Sulfuras/
-      SulfurasQualityUpdater.new(item)
-    when /^Conjured/
-      ConjuredItemQualityUpdater.new(item)
-    else
-      NormalItemQualityUpdater.new(item)
+  class BaseItemQualityCalculator
+    MIN_QUALITY = 0
+    MAX_QUALITY = 50
+
+    def calculate_sell_in(item)
+      item.sell_in - 1
     end
-  end
-end
 
-class BaseItemQualityUpdater
-  attr_reader :item, :max_quality, :min_quality
+    def calculate_quality(item)
+      raise NotImplentedError
+    end
 
-  def initialize(item)
-    @item = item
-    @max_quality = 50
-    @min_quality = 0
-  end
+    def increase_quality_by(item, amount)
+      quality = item.quality + amount
+      quality > MAX_QUALITY ? MAX_QUALITY : quality
+    end
 
-  def update_quality
-    degrease_sell_in
-    set_new_quality
-  end
+    def degrease_quality_by(item, amount)
+      quality = item.quality - amount
+      quality < MIN_QUALITY ? MIN_QUALITY : quality
+    end
 
-  def degrease_sell_in
-    item.sell_in -= 1
-  end
-
-  def set_new_quality
-    raise NotImplementedError
-  end
-
-  def is_after_sell_date?
-    item.sell_in < 0
-  end
-
-  def increase_quality_by(amount)
-    item.quality += amount
-    item.quality = max_quality if item.quality > max_quality
-  end
-
-  def degrease_quality_by(amount)
-    item.quality -= amount
-    item.quality = min_quality if item.quality < min_quality
-  end
-end
-
-class NormalItemQualityUpdater < BaseItemQualityUpdater
-  def set_new_quality
-    is_after_sell_date? ? degrease_quality_by(2) : degrease_quality_by(1)
-  end
-end
-
-class ConjuredItemQualityUpdater < BaseItemQualityUpdater
-  def set_new_quality
-    is_after_sell_date? ? degrease_quality_by(4) : degrease_quality_by(2)
-  end
-end
-
-class AgedBrieQualityUpdater < BaseItemQualityUpdater
-  def set_new_quality
-    is_after_sell_date? ? increase_quality_by(2) : increase_quality_by(1)
-  end
-end
-
-class BackstagePassQualityUpdater < BaseItemQualityUpdater
-  def set_new_quality
-    if is_after_sell_date?
-      item.quality = 0
-    elsif is_very_close_to_sell_date?
-      increase_quality_by(3)
-    elsif is_medium_close_to_sell_date?
-      increase_quality_by(2)
-    else
-      increase_quality_by(1)
+    def is_after_sell_date?(item)
+      item.sell_in < 0
     end
   end
 
-  def is_very_close_to_sell_date?
-    item.sell_in < 5
+  class NormalItemQualityCalculator < BaseItemQualityCalculator
+    def calculate_quality(item)
+      amount = is_after_sell_date?(item) ? 2 : 1
+      degrease_quality_by(item, amount)
+    end
   end
 
-  def is_medium_close_to_sell_date?
-    item.sell_in < 10
+  class ConjuredItemQualityCalculator < BaseItemQualityCalculator
+    def calculate_quality(item)
+      amount = is_after_sell_date?(item) ? 4 : 2
+      degrease_quality_by(item, amount)
+    end
   end
-end
 
-class SulfurasQualityUpdater < BaseItemQualityUpdater
-  def update_quality
-    # does nothing
+  class AgedBrieQualityCalculator < BaseItemQualityCalculator
+    def calculate_quality(item)
+      amount = is_after_sell_date?(item) ? 2 : 1
+      increase_quality_by(item, amount)
+    end
+  end
+
+  class BackstagePassQualityCalculator < BaseItemQualityCalculator
+    def calculate_quality(item)
+      if is_after_sell_date?(item)
+        0
+      elsif is_very_close_to_sell_date?(item)
+        increase_quality_by(item, 3)
+      elsif is_medium_close_to_sell_date?(item)
+        increase_quality_by(item, 2)
+      else
+        increase_quality_by(item, 1)
+      end
+    end
+
+    def is_very_close_to_sell_date?(item)
+      item.sell_in < 5
+    end
+
+    def is_medium_close_to_sell_date?(item)
+      item.sell_in < 10
+    end
+  end
+
+  class SulfurasQualityCalculator < BaseItemQualityCalculator
+    def calculate_sell_in(item)
+      item.sell_in
+    end
+
+    def calculate_quality(item)
+      item.quality
+    end
+  end
+
+  CALCULATORS = { "Backstage passes to a TAFKAL80ETC concert" => BackstagePassQualityCalculator.new,
+                  "Aged Brie" => AgedBrieQualityCalculator.new,
+                  "Sulfuras, Hand of Ragnaros" => SulfurasQualityCalculator.new,
+                  "Conjured Mana Cake" => ConjuredItemQualityCalculator.new }
+
+  DEFAULT_CALCULATOR = NormalItemQualityCalculator.new
+
+  def self.get_quality_calculator_for(item)
+    CALCULATORS.fetch(item.name, DEFAULT_CALCULATOR)
   end
 end
 
